1cc58032db3317ae8a5e971666fc6347de484d70
Robert Li <li.robertj+git@gmail.com>
2013-09-21 22:23:50 -0400

    Add an installation script
    Script will add symlinks for every item in the current working
    directory except for "install.sh" and "README.md"

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for file in *; do
+    target="$HOME/.$file"
+    echo "-$target-$file-"
+    if [ -e "$target" ]; then
+        if [ ! -L "$target" ]; then
+            echo "WARNING: $target exists but is not a symlink!"
+        fi
+    else
+        if [ "$file" != 'install.sh' ] && [ "$file" != 'README.md' ]; then
+            echo "Creating $target"
+            ln -s "$PWD/$file" "$target"
+        fi
+    fi
+done
