42cd4dabde845bf8cf6f348c229a3238042eb44f
Robert Li <li.robertj+github@gmail.com>
2016-09-02 10:43:17 -0400

    Remove Google-specific code

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,30 +1,3 @@
-;;;;; Google ;;;;;
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-(require 'google)
-(require 'google3)
-(require 'google-imports)
-
-;; Build using Blaze
-(require 'google3-build)
-(setq google-build-system "blaze")
-
-;; Grok
-(grok-init)
-
-;; Find File At Point
-(require 'google3-ffap)
-
-;;;;; End Google ;;;;;
-
-
-
 ;; When in windowed mode
 (when (display-graphic-p)
   ;; Set font
