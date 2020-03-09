14db218b7d909e3d7570abbf3857ba0fe11f734e
Robert Li <robert.li@datadoghq.com>
2020-03-09 19:14:34 -0400

    Inhibit buffer list when opening multiple files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -72,15 +72,8 @@
 ;; Default window split behavior
 (setq split-width-threshold 80)
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+;; Inhibit buffer list when opening multiple files
+(setq inhibit-startup-buffer-menu t)
+
+;; Inhibit startup screen
+(setq inhibit-startup-screen t)
